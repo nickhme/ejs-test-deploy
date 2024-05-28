f9c1ac014b944213b251d15eba1231e37063005c
Nick Hayes <nhayes559@gmail.com>
2024-05-28 18:37:21 +0100

    in dunno

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -12,7 +12,7 @@ const authRouter = require("../../controllers/authController");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../../views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json());
